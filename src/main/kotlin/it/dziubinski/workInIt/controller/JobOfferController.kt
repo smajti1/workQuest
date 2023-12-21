06121b708eb387c4695a3113f90cc5bb20efe9ae
Hubert Dziubiński <hubert@dziubinski.it>
2023-12-21 16:14:34 +0100

    set server cors allowedOrigins to *

--- src/main/kotlin/it/dziubinski/workInIt/controller/JobOfferController.kt
+++ src/main/kotlin/it/dziubinski/workInIt/controller/JobOfferController.kt
@@ -4,6 +4,7 @@ import it.dziubinski.workInIt.model.JobOfferCount
 import it.dziubinski.workInIt.model.JobPortal
 import it.dziubinski.workInIt.repository.JobOfferCountRepository
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,5 +15,11 @@ class JobOfferController(val jobOfferCountRepository: JobOfferCountRepository) {
     @GetMapping
     fun list(): Map<JobPortal, List<JobOfferCount>> {
         return jobOfferCountRepository.findAll().groupBy { it.jobPortal }
+    }
+
+    @GetMapping("/{jobPortal}")
+    fun list(@PathVariable("jobPortal") jobPortal: JobPortal): Map<String, Int> {
+        return jobOfferCountRepository.findByJobPortalOrderByCreatedAt(jobPortal)
+            .associate { it.createdAt.toLocalDate().toString() to it.count }
     }
 }
--- src/main/kotlin/it/dziubinski/workInIt/repository/JobOfferCountRepository.kt
+++ src/main/kotlin/it/dziubinski/workInIt/repository/JobOfferCountRepository.kt
@@ -1,8 +1,10 @@
 package it.dziubinski.workInIt.repository
 
 import it.dziubinski.workInIt.model.JobOfferCount
+import it.dziubinski.workInIt.model.JobPortal
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
 interface JobOfferCountRepository : JpaRepository<JobOfferCount, UUID> {
+    fun findByJobPortalOrderByCreatedAt(jobPortal: JobPortal): Iterable<JobOfferCount>
 }
