8768509a5bf80306e61a03b0fea2ba59e3abe0bc
Hubert Dziubiński <hubert@dziubinski.it>
2024-01-24 18:31:27 +0100

    selenium change browser to chrome add options to optimize memory/cpu usage

--- src/main/kotlin/it/dziubinski/workInIt/cron/JobOfferScrapWebPageCronAbstract.kt
+++ src/main/kotlin/it/dziubinski/workInIt/cron/JobOfferScrapWebPageCronAbstract.kt
@@ -6,7 +6,7 @@ import it.dziubinski.workInIt.model.JobOfferCount
 import it.dziubinski.workInIt.model.JobPortal
 import it.dziubinski.workInIt.repository.JobOfferCountRepository
 import org.openqa.selenium.WebDriver
-import org.openqa.selenium.firefox.FirefoxOptions
+import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.remote.RemoteWebDriver
 import java.net.URI
 
@@ -21,18 +21,28 @@ abstract class JobOfferScrapWebPageCronAbstract(
         val request = urlBuilder.apply { this.jobCategory = jobCategory; this.city = city }.build()
         println(request.cUrlString())
 
-        val firefoxOptions = FirefoxOptions()
-        firefoxOptions.addArguments("--disable-gpu")
-        firefoxOptions.addArguments("--headless")
-        firefoxOptions.addArguments("--no-sandbox")
-        firefoxOptions.addArguments("--disable-dev-shm-usage")
-        firefoxOptions.addArguments("--disable-extensions")
-        
-        val driver = RemoteWebDriver(URI(SELENIUM_URL).toURL(), firefoxOptions)
+        val options = ChromeOptions()
+        // https://chromium.googlesource.com/chromium/src/+/refs/tags/122.0.6261.4/components/content_settings/core/common/pref_names.h
+        val prefs = mapOf(
+            "profile.managed_default_content_settings.images" to 2, // block image loading
+        )
+        options.setExperimentalOption("prefs", prefs)
+        options.addArguments("window-size=1280,600")
+        options.addArguments("disable-infobars")
+        options.addArguments("--no-sandbox")
+        options.addArguments("--disable-extensions")
+        options.addArguments("--disable-application-cache")
+        options.addArguments("--disable-dev-shm-usage")
+        options.addArguments("--disable-extensions")
+        options.addArguments("--disable-gpu")
+
+        val driver = RemoteWebDriver(URI(SELENIUM_URL).toURL(), options)
         driver.get(request.url.toString())
 
         val offerCount = getCountFromWebPage(driver)
         saveNewJobOfferCount(jobPortal, jobCategory, city, offerCount)
+        // val screenshot = driver.getScreenshotAs(OutputType.FILE)
+        // FileUtils.copyFile(screenshot, File("screenshots/${LocalDate.now()}-${jobPortal}-${jobCategory}-${city}.png"))
 
         driver.quit()
     }
